0f5bd311bf2f687c9d7306116665183b01bfe761
Yuval Ben-Hayun <ybenhayun@gmail.com>
2022-02-06 12:50:14 -0500

    guesses width now responsive

--- wordl.js
+++ wordl.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
         filterList();
     });
 
-    $("#myRange").click(function() {
+    $("#myRange").change(function() {
         changeLength();
         setLength();
         makeTables();
