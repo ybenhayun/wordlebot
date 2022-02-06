3b8c2f7928479934477562502562007ec3b8df3b
Yuval Ben-Hayun <ybenhayun@gmail.com>
2022-02-06 14:07:00 -0500

    cleaned code

--- wordl.js
+++ wordl.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
         filterList();
     });
 
-    $("#myRange").change(function() {
+    $("#num_letters").change(function() {
         changeLength();
         setLength();
         makeTables();
@@ -50,35 +50,33 @@ $(document).ready(function() {
 });
 
 function makeTables() {
-    var correct_tbl = "<table id = 'corr'><tr>";
+    var correct_letters = "<table id = 'corr'><tr>";
 
     for(var i = 0; i < word_length; i++){
-        correct_tbl += "<td><input onkeypress = 'return /[a-z]/i.test(event.key)' oninput= 'this.value = this.value.toUpperCase()' type = 'letter' maxlength = '1 size = '1'></td>";
+        correct_letters += "<td><input onkeypress = 'return /[a-z]/i.test(event.key)' oninput= 'this.value = this.value.toUpperCase()' type = 'letter' maxlength = '1 size = '1'></td>";
     }
 
-    correct_tbl +=  '</tr></table>';
-    document.getElementById("correct").getElementsByClassName("table")[0].innerHTML = correct_tbl;
+    correct_letters +=  '</tr></table>';
+    document.getElementById("correct").getElementsByClassName("table")[0].innerHTML = correct_letters;
 
-    var wrong_spots_tbl = "<table id = 'wrong'>";
+    var wrong_spots = "<table id = 'wrong'>";
 
     for (var i = 0; i < guesses; i++) {
-        wrong_spots_tbl += '<tr>';
+        wrong_spots += '<tr>';
         for (var j = 0; j < word_length; j++) {
-            wrong_spots_tbl += "<td><input onkeypress = 'return /[a-z]/i.test(event.key)' oninput= 'this.value = this.value.toUpperCase()' type = 'letter' maxlength = '1 size = '1'></td>";
+            wrong_spots += "<td><input onkeypress = 'return /[a-z]/i.test(event.key)' oninput= 'this.value = this.value.toUpperCase()' type = 'letter' maxlength = '1 size = '1'></td>";
         }
-        wrong_spots_tbl += '</tr>';
+        wrong_spots += '</tr>';
     }
-    wrong_spots_tbl += '</table>';
+    wrong_spots += '</table>';
 
-    document.getElementById("wrong_spots").getElementsByClassName("table")[0].innerHTML = wrong_spots_tbl;
+    document.getElementById("wrong_spots").getElementsByClassName("table")[0].innerHTML = wrong_spots;
     document.getElementById("exclude").getElementsByClassName("table")[0].innerHTML = 
     "<table id = 'excl'><tr><td><input onkeypress = 'return /[a-z]/i.test(event.key)' oninput= 'this.value = this.value.toUpperCase()' type = 'letter' size = '1'></td></tr></table>";
-
-    // document.querySelectorAll("table").forEach(function(t) { t.style.width = word_length*3 + "rem"; });
 }
 
 function changeLength() {
-    word_length = document.getElementById("myRange").value;
+    word_length = document.getElementById("num_letters").value;
 }
 
 function setLength() {
@@ -94,15 +92,16 @@ function filterList() {
     wrongSpots(filtered);
     correctLetters(filtered);
     wrongLetters(filtered);
+
     var sorted = sortList(filtered);
 
     document.getElementById("count").innerHTML = sorted.length + " possible word" + (sorted.length != 1 ? "s" : "") + "."
     document.getElementById("list").innerHTML = "";
     
-    var best = sorted[0].rank;
+    var best_word = sorted[0].rank;
 
     for (var i = 0; i < sorted.length && i < list_size; i++) { 
-        if (sorted[i].rank == best) document.getElementById("list").innerHTML += "<span class = 'best'>" + sorted[i].word + "</span>";
+        if (sorted[i].rank == best_word) document.getElementById("list").innerHTML += "<span class = 'best_word'>" + sorted[i].word + "</span>";
         else break;
         
         if (i < sorted.length - 1) document.getElementById("list").innerHTML += ", ";
@@ -120,24 +119,25 @@ function sortList(list) {
     document.getElementById('best').innerHTML = "";
     if (!list.length) return [];
 
-    var alpha = [];
+    var alphabet = [];
     var sorted = [];
-    var top_letters = [];
+    var letters_ranked = [];
 
     for (var c = 65; c <= 90; c++) {
-        alpha[String.fromCharCode(c)] = [];
+        alphabet[String.fromCharCode(c)] = [];
         for (var i = 0; i < word_length+1; i++) {
-            alpha[String.fromCharCode(c)].push(0);
+            alphabet[String.fromCharCode(c)].push(0);
         }
     }
 
     var checked;
+
     for (var i = 0; i < list.length; i++) {
         checked = [];
         for (var j = 0; j < word_length; j++) {
-            alpha[list[i].charAt(j)][j]++;
+            alphabet[list[i].charAt(j)][j]++;
 
-            if (checked[list[i].charAt(j)] != true) alpha[list[i].charAt(j)][word_length]++;
+            if (checked[list[i].charAt(j)] != true) alphabet[list[i].charAt(j)][word_length]++;  // only counts letters once per word
             checked[list[i].charAt(j)] = true;
         }
 
@@ -145,13 +145,13 @@ function sortList(list) {
     }
 
     for (var i = 0; i < 26; i++) {
-        top_letters.push({letter:String.fromCharCode(i+65), score:alpha[String.fromCharCode(i+65)][word_length]});
+        letters_ranked.push({letter:String.fromCharCode(i+65), score:alphabet[String.fromCharCode(i+65)][word_length]});
     }
 
-    top_letters.sort((a, b) => (a.score <= b.score) ? 1 : -1);
+    letters_ranked.sort((a, b) => (a.score <= b.score) ? 1 : -1);
 
     for (var c = 0; c < 26; c++) {
-        document.getElementById('best').innerHTML += "<li>" + top_letters[c].letter + ":   " + parseFloat(top_letters[c].score/list.length*100).toFixed(2) + "%</li>";
+        document.getElementById('best').innerHTML += "<li>" + letters_ranked[c].letter + ":   " + parseFloat(letters_ranked[c].score/list.length*100).toFixed(2) + "%</li>";
     }
 
     checked = [];
@@ -160,8 +160,8 @@ function sortList(list) {
         for (var i = 0; i < sorted.length; i++) {
             for (var j = 0; j < word_length; j++) {
                 if (checked[i + " " + sorted[i].word.charAt(j)] == true) continue;  //no extra credit to letters with doubles
-                sorted[i].rank += alpha[sorted[i].word.charAt(j)][word_length];
-                // sorted[i].rank += alpha[sorted[i].word.charAt(j)][j];    // makes TARES top word
+                sorted[i].rank += alphabet[sorted[i].word.charAt(j)][word_length];
+                // sorted[i].rank += alphabet[sorted[i].word.charAt(j)][j];    // makes TARES top word
                 checked[i + " " + sorted[i].word.charAt(j)] = true;
             }
         }
@@ -170,31 +170,31 @@ function sortList(list) {
 
     sorted.sort((a, b) => (a.rank <= b.rank) ? 1 : -1);
 
-    var small_list = [];
+    var sub_list = [];
     var place = 0;
     var current = sorted[place].rank;
 
     for (var i = 0; i < sorted.length; i++) {
         if (sorted[i].rank == current && i < sorted.length - 1) { 
-            small_list.push(sorted[i]);
+            sub_list.push(sorted[i]);
         } else {
-            if (i == sorted.length - 1) small_list.push(sorted[i]);
-            for (var j = 0; j < small_list.length; j++) {
+            if (i == sorted.length - 1) sub_list.push(sorted[i]);
+            for (var j = 0; j < sub_list.length; j++) {
                 for (var k = 0; k < word_length; k++) {
-                    small_list[j].rank += alpha[small_list[j].word.charAt(k)][k];
+                    sub_list[j].rank += alphabet[sub_list[j].word.charAt(k)][k];
                 }
             } 
 
-            small_list.sort((a, b) => (a.rank <= b.rank) ? 1 : -1);
+            sub_list.sort((a, b) => (a.rank <= b.rank) ? 1 : -1);
 
-            for (var j = 0; j < small_list.length; j++) {
-                sorted[j+place] = small_list[j];
+            for (var j = 0; j < sub_list.length; j++) {
+                sorted[j+place] = sub_list[j];
             }
 
             current = sorted[i].rank;
             place = i;
-            small_list = [];
-            small_list.push(sorted[i]);
+            sub_list = [];
+            sub_list.push(sorted[i]);
         }
     }
 
@@ -217,25 +217,25 @@ function wrongLetters(list) {
 
 function wrongSpots(list) {
     var table = document.getElementById("wrong");
-    var includes = [];
+    var letters = [];
 
     for (var i = 0; i < guesses; i++) {
         for (var j = 0; j < word_length; j++) {
             var char = table.rows[i].cells[j].children[0].value;
-            if (char != '') includes.push({letter:char, place:j});
+            if (char != '') letters.push({letter:char, place:j});
         }
     }
 
-    if (includes.length) {
+    if (letters.length) {
         for (var i = 0; i < list.length; i++) {
-            for (var j = 0; j < includes.length; j++) {
-                if (!list[i].includes(includes[j].letter)) {
+            for (var j = 0; j < letters.length; j++) {
+                if (!list[i].includes(letters[j].letter)) {
                     list.splice(i, 1);
                     i--;
                     break;
                 }
 
-                if (list[i].charAt(includes[j].place) == includes[j].letter) {
+                if (list[i].charAt(letters[j].place) == letters[j].letter) {
                     list.splice(i, 1);
                     i--;
                     break;
@@ -247,17 +247,17 @@ function wrongSpots(list) {
 
 function correctLetters(list) {
     var table = document.getElementById('corr');
-    var includes = [];
+    var letters = [];
 
     for (var i = 0; i < word_length; i++) {
-        includes.push(table.rows[0].cells[i].children[0].value); //first column
+        letters.push(table.rows[0].cells[i].children[0].value); //first column
     }
 
-    if (includes.some(element => element != '')) {
+    if (letters.some(element => element != '')) {
         for (var i = 0; i < list.length; i++) {
             for (var j = 0; j < word_length; j++) {
-                if (includes[j] != '') {
-                    if (includes[j] != list[i].charAt(j)) { 
+                if (letters[j] != '') {
+                    if (letters[j] != list[i].charAt(j)) { 
                         list.splice(i, 1);
                         i--;
                         break;
